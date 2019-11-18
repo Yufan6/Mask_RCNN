8630b389a66069eb7bf03935a6bfc7d26e6d28ba
Yufan6 <z5071778@hotmail.com>
2019-11-18 09:52:47 -0700

    root_dir change

--- samples/demo.py
+++ samples/demo.py
@@ -9,7 +9,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("/home/yufan/PycharmProjects/Mask_RCNN")
+ROOT_DIR = os.path.abspath("/home/yufan/Mask_RCNN")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
